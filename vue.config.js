5d8aa7385f875b427454384480e7fd3e3be36cc4
h1romas4 <h1romas4@gmail.com>
2020-03-19 15:17:03 +0900

    fix publish path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/e2j-api/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/e2j-api-web/' : '/',
     "transpileDependencies": [
     "vuetify"
   ]
