3c4ffba2a2481430cebd83c48f33a3de5a7ffa07
h1romas4 <h1romas4@gmail.com>
2020-03-19 16:49:47 +0900

    for testing

---
+++ src/components/MameTan.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-toolbar
+          dark
+          color="teal"
+        >
+          <v-toolbar-title>E2J mametan API</v-toolbar-title>
+          <v-autocomplete
+            v-model="select"
+            :loading="loading"
+            :items="items"
+            :search-input.sync="search"
+            clearable
+            cache-items
+            class="mx-4"
+            filled
+            flat
+            hide-no-data
+            hide-details
+            label="All your base are belong to us."
+            solo-inverted
+          ></v-autocomplete>
+        </v-toolbar>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import { mamedic } from './mamedic';
+  export default {
+    name: "MameTan",
+    data () {
+      return {
+        loading: false,
+        items: [],
+        search: null,
+        select: null,
+        states: mamedic,
+      }
+    },
+    watch: {
+      search (val) {
+        val && val !== this.select && this.querySelections(val)
+      },
+    },
+    methods: {
+      querySelections (v) {
+        this.loading = true
+        this.items = this.states.filter(e => {
+          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+        })
+        this.loading = false
+      },
+    },
+  }
+</script>
