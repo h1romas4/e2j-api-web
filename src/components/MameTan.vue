1b97b3df22a9242a91197420f5491f7878fbe0eb
h1romas4 <h1romas4@gmail.com>
2020-03-24 21:51:38 +0900

    add e2j

--- src/components/MameTan.vue
+++ src/components/MameTan.vue
@@ -25,12 +25,30 @@
         </v-toolbar>
       </v-col>
     </v-row>
+    <v-timeline dense>
+      <v-timeline-item
+        v-for="(result, i) in results"
+        :key="i"
+        small
+      >
+        <template v-slot:opposite>
+          <span
+            :class="`headline font-weight-bold`"
+            v-text="result.release"
+          ></span>
+        </template>
+        <v-card class="elevation-2">
+          <v-card-title class="headline">{{ result.filename }}</v-card-title>
+          <v-card-text><pre>{{ result.content }}</pre></v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
   </v-container>
 </template>
 
 <script>
   import axios from 'axios';
-  import { apiBaseUrl, apiMametan } from './mametan';
+  import { apiBaseUrl, apiMametan, apiWhatsNewJ } from './mametan';
 
   export default {
     name: "MameTan",
@@ -40,26 +58,57 @@
         items: [],
         search: null,
         select: null,
-        states: [],
+        results: [],
+        memetan: [],
+        releases: [],
       }
     },
     watch: {
       search (val) {
-        val && val !== this.select && this.querySelections(val)
+        if(val) {
+          if(val !== this.select) this.querySelections(val)
+          if(this.memetan.indexOf(val) >= 0) this.queryWhatsNewJ(val)
+        }
       },
     },
     methods: {
       querySelections (v) {
-        this.items = this.states.filter(e => {
+        this.items = this.memetan.filter(e => {
           return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
         })
+      },
+      async queryWhatsNewJ (val) {
+        console.log("hit!")
+        this.loading = true
+        try {
+          const res1 = await axios.get(`${apiBaseUrl}${apiWhatsNewJ}`, {
+            params: { q: val }
+          })
+          this.results = []
+          res1.data.forEach((data) => {
+            this.results.push({
+              filename: data.filename,
+              release: "release",
+              content: data.content,
+              line: data.line
+            })
+          })
+          console.log(this.results)
+        } finally {
+          this.loading = false
+        }
       },
     },
     async mounted() {
       this.loading = true
-      const res = await axios.get(`${apiBaseUrl}${apiMametan}`)
-      this.states = res.data
-      this.loading = false
-    }
+      try {
+        const res1 = await axios.get(`${apiBaseUrl}${apiMametan}`)
+        this.memetan = res1.data
+        const res2 = await axios.get(`${apiBaseUrl}${apiMametan}`)
+        this.releases = res2.data
+      } finally {
+        this.loading = false
+      }
+    },
   }
 </script>
--- src/components/mametan.js
+++ src/components/mametan.js
@@ -1,2 +1,4 @@
 export const apiBaseUrl = "https://api.e2j.net";
 export const apiMametan = "/v1/mametan";
+export const apiRelases = "/v1/releases";
+export const apiWhatsNewJ = "/v1/whatsnewj";
